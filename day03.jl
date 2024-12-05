04450caf6256b0a6e9253d539e88b92c4e8af4fe
xpakx <55586470+xpakx@users.noreply.github.com>
2024-12-05 14:37:17 +0100

    feat(day03): solve second star

--- day03.jl
+++ day03.jl
@@ -3,3 +3,21 @@ file_content = read(filename, String)
 matches = collect(eachmatch(r"mul\((-?\d+),(-?\d+)\)",file_content))
 parsed_matches = [parse(Int, match.captures[1])*parse(Int, match.captures[2]) for match in matches]
 println(sum(parsed_matches))
+
+matches = collect(eachmatch(r"(mul\((-?\d+),(-?\d+)\))|(do\(\))|(don\'t\(\))",file_content))
+enabled = true
+result = 0
+for match in matches
+	if !isnothing(match.captures[1])  # mul()
+		if enabled
+			num1 = parse(Int, match.captures[2])
+			num2 = parse(Int, match.captures[3])
+			global result += num1 * num2
+		end
+	elseif !isnothing(match.captures[4])  # do()
+		global enabled = true
+	elseif !isnothing(match.captures[5])  # don't()
+		global enabled = false
+    end
+end
+println(result)
