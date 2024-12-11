62a04f5cd57d4bc48314e229c38f93085dcb672b
xpakx <55586470+xpakx@users.noreply.github.com>
2024-12-11 18:21:24 +0100

    feat(day11): solve second star

--- day11.jl
+++ day11.jl
@@ -40,9 +40,44 @@ function transform(stones::Vector{Int})::Vector{Int}
 	return result
 end
 
-
-for blink in 1:25
-	global stones = transform(stones)
+function blinks(stones::Vector{Int}, blink_num::Int)::Int
+	for _ in 1:blink_num
+		stones = transform(stones)
+	end
+	return length(stones)
 end
 
-println(length(stones))
+
+function transform(stones::Dict{Int, Int})::Dict{Int, Int}
+	new_stones = Dict{Int, Int}()
+	for (stone, stone_count) in stones
+		new_stone = transform(stone)
+		for s in new_stone
+			if haskey(new_stones, s)
+				new_stones[s] += stone_count
+			else
+				new_stones[s] = stone_count
+			end
+		end
+	end
+	return new_stones
+end
+
+function blink_reduce(stones::Vector{Int}, blink_num::Int)::Int
+	stone_count = Dict{Int, Int}() # stone -> count
+	for stone in stones
+		if haskey(stone_count, stone)
+			stone_count[stone] += 1
+		else
+			stone_count[stone] = 1
+		end
+	end
+	for _ in 1:blink_num
+		stone_count = transform(stone_count)
+	end
+	return sum(values(stone_count))
+end
+
+
+println(blinks(stones, 25))
+println(blink_reduce(stones, 75))
