70f615693d3936f96b4d6bbd64695f690491d5b0
xpakx <55586470+xpakx@users.noreply.github.com>
2024-12-12 11:40:04 +0100

    feat(day12): trace border

---
+++ day12.jl
@@ -0,0 +1,91 @@
+filename = "data/data12.txt"
+file_content = strip(read(filename, String))
+content = split(file_content, '\n')
+chars = map(x -> collect(x), content)
+
+function is_within_bounds(terrain::Vector{Vector{Char}}, start::Tuple{Int, Int})
+    row, col = start
+    return 1 <= row <= size(terrain, 1) && 1 <= col <= size(terrain[1], 1)
+end
+
+function get(terrain::Vector{Vector{Char}}, start::Tuple{Int, Int})::Char
+	return terrain[start[1]][start[2]]
+end
+
+function check(terrain::Vector{Vector{Char}}, pos::Tuple{Int, Int}, delta::Tuple{Int, Int}, letter::Char)::Bool
+	next = pos .+ delta
+	return is_within_bounds(terrain, next) && get(terrain, next) == letter
+end
+
+function add(a::Tuple{Int, Int}, b::Tuple{Int, Int})
+    (a[1] - b[1], a[2] + b[2])
+end
+
+function rotate(dir)
+    dx, dy = dir
+    return (-dy, dx)
+end
+
+function revert(dir)
+    dx, dy = dir
+    return (-dx, -dy)
+end
+
+function to_dir(dir)
+	if dir == (1,0)
+		return "up"
+	elseif dir == (-1,0)
+		return "down"
+	elseif dir == (0,1)
+		return "right"
+	else 
+		return "left"
+	end
+end
+
+function trace(start::Tuple{Int, Int}, terrain::Vector{Vector{Char}})::Int
+	letter = get(terrain, start)
+	perimeter = 0
+	pos = start
+	dir = (0, 1)
+	side = (1, 0)
+	while (pos != start || side != (1, 0)) || perimeter == 0
+		println("$pos, side: $side (", to_dir(side), "), dir: $dir (", to_dir(dir), ")")
+		perimeter += 1
+
+		next = rotate(side)
+		println(to_dir(next))
+		test_pos = add(pos, next)
+		println("to test: ",test_pos)
+		if !is_within_bounds(terrain, test_pos) || get(terrain, test_pos) != letter
+			println("normal")
+			side = next
+			dir = rotate(dir)
+			continue
+		end
+		diag = add(add(pos, dir), side)
+		println("diag", diag)
+		if is_within_bounds(terrain, diag) && get(terrain, diag) == letter
+			println("diag")
+			pos = diag
+			dir = revert(rotate(dir))
+			side = revert(rotate(side))
+			continue
+		end
+		pos = add(pos, dir)
+		println("side",pos)
+	end
+	return perimeter
+
+end
+
+println(chars)
+result = 0
+result += trace((1,1), chars)
+result += trace((2,1), chars)
+result += trace((2,3), chars)
+result += trace((2,4), chars)
+result += trace((4,1), chars)
+
+println(result)
+
