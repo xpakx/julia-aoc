7a666c28c3161d0418c9d74499b09752ee7b41ec
xpakx <55586470+xpakx@users.noreply.github.com>
2024-12-13 16:27:09 +0100

    feat(day13): solve first star

--- day13.jl
+++ day13.jl
@@ -8,6 +8,27 @@ function parse_game(text::String)::Game
 		return ((nums[1], nums[2]), (nums[3], nums[4]), (nums[5], nums[6]))
 end
 
+function solve(game::Game)::Tuple{Int, Int}
+	(a₁, a₂) = game[1]
+	(b₁, b₂) = game[2]
+	(x, y) = game[3]
+	det = a₁*b₂ - a₂*b₁
+	if det == 0
+		return (-1, -1) #TODO?
+	end
+	n_numerator = x*b₂ - y*b₁
+	m_numerator = y*a₁ - x*a₂
+
+	if n_numerator % det != 0 || m_numerator % det !=0
+		return (-1, -1)
+	end
+	return (n_numerator/det, m_numerator/det)
+end
+
+cost(solution) = 3*solution[1] + solution[2] 
+
 games = map(parse_game, content)
-println(games)
+solutions = map(solve, games)
+result = sum(map(cost, filter(x -> x[1] > 0, solutions)))
+println(result)
 
