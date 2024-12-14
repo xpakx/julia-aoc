b87a574c0b06917fb79591296846e06f19844bd4
xpakx <55586470+xpakx@users.noreply.github.com>
2024-12-14 12:20:20 +0100

    feat(day14): solve first star

--- day14.jl
+++ day14.jl
@@ -13,6 +13,27 @@ function read_file(filename::String)::Vector{Robot}
 	return data
 end
 
+function simulate(robot::Robot, steps::Int, width::Int, height::Int)::Tuple{Int, Int}
+	(p₁, p₂) = robot[1]
+	(v₁, v₂) = robot[2]
+	r₁ = mod(p₁ + steps*v₁, width)
+	r₂ = mod(p₂ + steps*v₂, height)
+	return (r₁, r₂)
+end
+
+function reduce_quadrants(positions, width, height)
+    (i, j) = ((width-1)/2, (height-1)/2)
+    a = sum(x > i && y > j for (x, y) in positions)
+    b = sum(x < i && y > j for (x, y) in positions)
+    c = sum(x < i && y < j for (x, y) in positions)
+    d = sum(x > i && y < j for (x, y) in positions)
+    return (a, b, c, d)
+end
+
+width = 101
+height = 103
 filename = "data/data14.txt"
 data = read_file(filename)
-println(data)
+positions = map(x -> simulate(x, 100, width, height), data)
+results  = reduce_quadrants(positions, width, height)
+println(reduce(*, results))
