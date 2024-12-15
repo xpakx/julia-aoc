558b23a160f40770d937413dd0fd27fc06dcaa83
xpakx <55586470+xpakx@users.noreply.github.com>
2024-12-15 14:20:42 +0100

    feat(day15): simulate movement

--- day15.jl
+++ day15.jl
@@ -24,6 +24,51 @@ function read_file(filename::String)::Tuple{Map, Vector{Dir}}
 	return (map_data, dirs)
 end
 
+function dir_to_coord(dir::Dir)::Tuple{Int, Int}
+	if dir == Up return (-1, 0) elseif dir == Right return (0, 1) elseif dir == Left return (0, -1) elseif dir == Down return (1, 0) end
+end
+
+function get(board::Map, pos::Tuple{Int, Int})::Field
+	return board[pos[1]][pos[2]]
+end
+
+function set!(board::Map, pos::Tuple{Int, Int}, value::Field)
+	board[pos[1]][pos[2]] = value
+end
+
+function step!(board::Map, dirs::Vector{Dir}, step_num::Int, player::Tuple{Int, Int})
+	num = step_num % length(dirs)
+	dir = dirs[num]
+	dir_coord = dir_to_coord(dir)
+	new_pos = player .+ dir_coord
+	if get(board, new_pos) == Wall
+		return player
+	elseif get(board, new_pos) == Empty
+		set!(board, player, Empty)
+		set!(board, new_pos, Player)
+		return new_pos
+	elseif get(board, new_pos) == Box
+		pos2 = new_pos .+ dir_coord
+		while get(board, pos2) == Box
+			pos2 = pos2 .+ dir_coord
+		end
+		if get(board, pos2) == Wall
+			return player
+		end
+		set!(board, player, Empty)
+		set!(board, new_pos, Player)
+		set!(board, pos2, Box)
+		return new_pos
+	end
+end
+
+
 filename = "data/data15.txt"
 data = read_file(filename)
+player = [(i, j) for (i, row) in enumerate(data[1]) for (j, val) in enumerate(row) if val == Player][1]
 println(data)
+println(player)
+for i in 1:10
+	global player = step!(data[1], data[2], i, player)
+	println(player)
+end
