5a91730e060242c0c565032b25b62781c951dd80
xpakx <55586470+xpakx@users.noreply.github.com>
2024-12-15 15:49:32 +0100

    feat(day15): solve second star

--- day15.jl
+++ day15.jl
@@ -75,8 +75,149 @@ function calculate_gps(board::Map)
 	return sum(values)
 end
 
+function get_wide_map(board::Map)::Tuple{Map, Dict{Tuple{Int, Int}, Vector{Tuple{Int, Int}}}}
+	boxes = Dict{Tuple{Int, Int}, Vector{Tuple{Int, Int}}}()
+	new_board = Vector{Vector{Field}}()
+	for (i, row) in enumerate(board)
+		new_row = Vector{Field}()
+		for (j, val) in enumerate(row)
+			if val == Empty || val == Wall
+				push!(new_row, val)
+				push!(new_row, val)
+			elseif val == Box
+				push!(new_row, Box)
+				push!(new_row, Box)
+				positions = [(i,2*j-1), (i,2*j)]
+				boxes[(i,2*j)] = positions
+				boxes[(i,2*j-1)] = positions
+			elseif val == Player
+				push!(new_row, Player)
+				push!(new_row, Empty)
+			end
+		end
+		push!(new_board, new_row)
+	end
+	return (new_board, boxes)
+end
+
+function get_wall_tree(board, dict, player, dir_coord)::Tuple{Bool, Set{Tuple{Int, Int}}}
+	walls = Set{Tuple{Int,Int}}()
+	positions = [player]
+	while length(positions) > 0 
+		pos = pop!(positions)
+		new_pos = pos .+ dir_coord
+		if get(board, new_pos) == Wall
+			return (true, Set{Tuple{Int,Int}}())
+		elseif get(board, new_pos) == Box
+			box = dict[new_pos]
+			push!(walls, box[1])
+			push!(walls, box[2])
+			if dir_coord[1] === 0
+				if box[1] != pos
+					push!(positions, box[1])
+				else
+					push!(positions, box[2])
+				end
+			else
+				push!(positions, box[1])
+				push!(positions, box[2])
+			end
+		end
+	end
+	return (false, walls)
+end
+
+function update_walls!(board, boxes, dict, dir)
+	boxes_for_dict = Vector{Vector{Tuple{Int, Int}}}()
+	for box in boxes
+		set!(board, box, Empty)
+		if haskey(dict, box)
+			box_data = dict[box]
+			pop!(dict, box_data[1])
+			pop!(dict, box_data[2])
+			new_data = map(x -> x .+ dir, box_data)
+			push!(boxes_for_dict, new_data)
+		end
+	end
+	for data in boxes_for_dict
+		dict[data[1]] = data
+		dict[data[2]] = data
+	end
+	new_boxes = map(x -> x .+ dir, collect(boxes))
+	for box in new_boxes
+		set!(board, box, Box)
+	end
+end
+
+
+function step2!(board::Map, dirs::Vector{Dir}, dict, step_num::Int, player::Tuple{Int, Int})
+	num = ((step_num-1) % length(dirs)) + 1
+	dir = dirs[num]
+	dir_coord = dir_to_coord(dir)
+	new_pos = player .+ dir_coord
+	if get(board, new_pos) == Wall
+		return player
+	elseif get(board, new_pos) == Empty
+		set!(board, player, Empty)
+		set!(board, new_pos, Player)
+		return new_pos
+	elseif get(board, new_pos) == Box
+		pos2 = new_pos .+ dir_coord
+		(has_wall, boxes) = get_wall_tree(board, dict, player, dir_coord)
+		if has_wall
+			return player
+		end
+		update_walls!(board, boxes, dict, dir_coord)
+		set!(board, player, Empty)
+		set!(board, new_pos, Player)
+		return new_pos
+	end
+end
+
+function print_board(board::Map)
+	for (i, row) in enumerate(board)
+		for (j, val) in enumerate(row)
+			if val == Player
+				print("@")
+			elseif val == Wall
+				print("#")
+			elseif val == Box
+				print("O")
+			else 
+				print(".")
+			end
+		end
+		println()
+	end
+end
+
+function simulate2!(board::Map, dirs::Vector{Dir}, dict)
+	player = [(i, j) for (i, row) in enumerate(board) for (j, val) in enumerate(row) if val == Player][1]
+	for i in eachindex(dirs)
+		player = step2!(board, dirs, dict, i, player)
+	end
+end
+
+function calculate_gps2(dict)
+	visited = Set{Tuple{Int,Int}}()
+	result = 0
+	for (key, value) in dict
+		if !(key in visited)
+			(i, j) = value[1]
+			result += (i-1)*100 + j-1
+			push!(visited, value[1])
+			push!(visited, value[2])
+		end
+	end
+	return result
+end
+
 filename = "data/data15.txt"
 (board, dirs) = read_file(filename)
 simulate1!(board, dirs)
 println(calculate_gps(board))
 
+(board, dirs) = read_file(filename)
+(board2, dict) = get_wide_map(board)
+simulate2!(board2, dirs, dict)
+println(calculate_gps2(dict))
