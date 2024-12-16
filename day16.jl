d49bafa721336ef060e8fc4290a70985851d9e6d
xpakx <55586470+xpakx@users.noreply.github.com>
2024-12-16 15:35:28 +0100

    feat(day16): solve second star

--- day16.jl
+++ day16.jl
@@ -29,24 +29,72 @@ end
 
 const Pair = Tuple{Tuple{Int,Int},Tuple{Int,Int}}
 
-function dijkstra(board::Map, start::Tuple{Int, Int})::Int
+function print_board(board::Map, seats::Set{Tuple{Int, Int}})
+	for (i, row) in enumerate(board)
+		for (j, val) in enumerate(row)
+			if val == Wall
+				print("#")
+			elseif (i,j) in seats
+				print("O")
+			else 
+				print(".")
+			end
+		end
+		println()
+	end
+end
+
+function collect_seats(old_seats::Set{Tuple{Int, Int}}, pre::Dict{Pair, Vector{Pair}}, node, dir, start)::Set{Tuple{Int, Int}}
+	seats = Set{Tuple{Int, Int}}()
+	union!(seats, old_seats)
+	x = [(node, dir)]
+	while length(x) > 0
+		a = pop!(x)
+		if a in old_seats
+			continue
+		end
+		push!(seats, a[1])
+		if a[1] == start
+			continue
+		end
+		append!(x, pre[a])
+	end
+	return seats
+end
+
+function dijkstra(board::Map, start::Tuple{Int, Int})::Tuple{Int,Int}
 	Q = PriorityQueue{Pair, Int}()
 	d = Dict{Pair, Int64}()
-	pre = Dict{Pair, Pair}()
+	pre = Dict{Pair, Vector{Pair}}()
 
 	dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
 	enqueue!(Q, (start, (0, -1)) => 0)
 	d[(start, (0, -1))] = 0
 
+	results = 0
+	shortest = 0
+	seats = Set{Tuple{Int, Int}}()
+
 	while !isempty(Q)
 		node, dir = dequeue!(Q)
+		if get(board, node) == End
+			len = d[(node, dir)]
+			if shortest == 0
+				shortest = len
+			end
+			if len != shortest
+				return (shortest, length(seats))
+			end
+			seats = collect_seats(seats, pre, node, dir, start)
+			results += 1
+			continue
+		end
+
 		neighbors = map(x -> x .+ node, dirs)
 		for n in neighbors
 			if get(board, n) == Wall
 				continue
-			elseif get(board, n) == End
-				return d[(node, dir)] + 1
 			end
 			d1 = d2 = typemax(Int)
 			dir2 = node .- n
@@ -63,14 +111,18 @@ function dijkstra(board::Map, start::Tuple{Int, Int})::Int
 			if dir2 .* (-1, -1) == dir
 				dist += 1000
 			end
-			if d2 > d1 + dist
+			if d2 >= d1 + dist
 				if haskey(Q, (n, dir2))
 					Q[(n, dir2)] = d1 + dist
 				else
 					enqueue!(Q, (n, dir2) => d1 + dist)
 				end
 				d[(n, dir2)] = d1 + dist
-				pre[(n, dir2)] = (node, dir)
+				if haskey(pre, (n, dir2))
+					push!(pre[(n, dir2)], (node, dir))
+				else
+					pre[(n, dir2)] = [(node, dir)]
+				end
 			end
 		end
 	end
