1a508c87db0bf896fa13c330cd0d00fd4d888b03
xpakx <55586470+xpakx@users.noreply.github.com>
2024-12-21 08:53:29 +0100

    feat(day21): solve first star

--- day21.jl
+++ day21.jl
@@ -28,25 +28,52 @@ function calc_distances(panel::Panel)::Dict{Char, Pos}
 	return dist
 end
 
-function shortest_sequence(panel::Dict{Char, Pos}, code::String)::String
+function optimal_order(dx::Int, dy::Int)::String
+	result = ""
+	if dx > 0
+		result *= repeat("<", dx)
+	end
+	if dy < 0
+		result *= repeat("v", -dy)
+	end
+	if dy > 0
+		result *= repeat("^", dy)
+	end
+	if dx < 0
+		result *= repeat(">", -dx)
+	end
+	return result
+end
+
+function up_first(dx::Int, dy::Int)::String
+	return repeat("^", dy) * repeat(dx>0 ? "<" : ">", abs(dx))
+end
+
+function down_first(dx::Int, dy::Int)::String
+	return repeat("v", -dy) * repeat(dx>0 ? "<" : ">", abs(dx))
+end
+
+function right_first(dx::Int, dy::Int)::String
+	return repeat(">", -dx) * repeat(dy>0 ? "^" : "v", abs(dy))
+end
+
+function shortest_sequence(panel::Dict{Char, Pos}, code::String, numeric::Bool=true)::String
 	curr = 'A'
 	result = ""
+	empty = panel[' ']
 	for letter in collect(code)
 		from = panel[curr]
 		to = panel[letter]
 		dy = from[1] - to[1]
 		dx = from[2] - to[2]
-		if dx > 0
-			result *= repeat("<", dx)
-		end
-		if dy < 0
-			result *= repeat("v", -dy)
-		end
-		if dy > 0
-			result *= repeat("^", dy)
-		end
-		if dx < 0
-			result *= repeat(">", -dx)
+		if !numeric && to[2] == empty[2] && from[1] == empty[1] && dy > 0
+			result *= up_first(dx, dy)
+		elseif numeric && to[2] == empty[2] && from[1] == empty[1] && dy < 0
+			result *= down_first(dx, dy)
+		elseif to[1] == empty[1] && from[2] == empty[2] && dx < 0
+			result *= right_first(dx, dy)
+		else
+			result *= optimal_order(dx, dy)
 		end
 		result *= 'A'
 		curr = letter
@@ -55,7 +82,7 @@ function shortest_sequence(panel::Dict{Char, Pos}, code::String)::String
 end
 
 function calc_sequences(nav::Dict{Char, Pos}, main::Dict{Char, Pos}, code::String)::Int
-	res = shortest_sequence(main, code)
+	res = shortest_sequence(main, code, false)
 	res = shortest_sequence(nav, res)
 	res = shortest_sequence(nav, res)
 	len = length(res)
