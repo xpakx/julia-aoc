ef07377814fed7e605bd334167d1ded045d27b6d
xpakx <55586470+xpakx@users.noreply.github.com>
2024-12-22 06:23:42 +0100

    feat(day22): solve first star

--- day22.jl
+++ day22.jl
@@ -4,6 +4,26 @@ function get_seeds(filename::String)::Vector{Int}
 	return map(x -> parse(Int, String(x)), data)
 end
 
+function nextnum(num::Int)::Int
+	a = prune(mix(num, num*64))
+	b = prune(mix(a, a ÷ 32))
+	return prune(mix(b, b * 2048))
+end
+
+mix(secret::Int, num::Int)::Int = secret ⊻ num
+prune(num::Int)::Int = mod(num, 16777216)
+
+function sim(num::Int, n::Int)::Int
+	for _ in 1:n
+		num = nextnum(num)
+	end
+	return num
+end
+
 filename = "data/data22.txt"
 seeds = get_seeds(filename)
 println(seeds)
+
+result = map(a -> sim(a, 2000), seeds)
+println(result)
+println(sum(result))
